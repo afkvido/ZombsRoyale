284b75680e48100b519bcdcae3f0d9dcff1da509
gemsvido <gemsvido@gmail.com>
2022-11-19 23:11:50 -0800

    still not workign

--- src/noAds.ts
+++ src/noAds.ts
@@ -3,15 +3,21 @@
  *  This is not a replacement for uBlock Origin, however it does fix the annoying ads in ZombsRoyale.
  */
 
-// @ts-expect-error
-let game = window.game;
+
+ document.addEventListener("DOMContentLoaded", function () {
+
+    alert("Working.");
+
+    setInterval(() => {
+        try  {
+            // @ts-expect-error
+            window["game"]["shouldShowAds"] = false;
+        } catch (err) {
+            console.error(err)
+        }
+    }, 1000);
+
+});
 
 
-let adsChecker = setInterval(() => {
-    try  {
-       game.shouldShowAds = false; 
-    } catch (err) {
-        console.error(err)
-    }
-}, 1000);
 
